f20780f9ecc88df86f2c1bc54dac9619cd5dde98
Teno Siswono <tennosys@gmail.com>
2021-06-30 15:40:55 +0700

    module 5

---
+++ react-fundamentals/5-node-package-with-react/excercise/README.md
@@ -0,0 +1,1 @@
+# In this folder you will create new node packages
---
+++ react-fundamentals/5-node-package-with-react/excercise/src/app.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+let HelloWorld = <h1>Hello, world!</h1>
+
+ReactDOM.render(HelloWorld, document.getElementById('root'))
